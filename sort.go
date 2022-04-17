9d2b0fea866122cf97eaf14b0051a1fe343075a2
Rizal Widyarta Gowandy <rizal.gow@gmail.com>
2022-04-17 15:41:29 +0700

    Add Sort byPrevRun

--- sort.go
+++ sort.go
@@ -18,3 +18,22 @@ func (s byNextRun) Less(i, j int) bool {
 	}
 	return s[i].Next.Before(s[j].Next)
 }
+
+// byPrevRun is a wrapper for sorting the entry array by prev run time
+// (with zero time at the end).
+type byPrevRun []StatusData
+
+func (s byPrevRun) Len() int      { return len(s) }
+func (s byPrevRun) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s byPrevRun) Less(i, j int) bool {
+	// Two zero times should return false.
+	// Otherwise, zero is "greater" than any other time.
+	// (To sort it at the end of the list.)
+	if s[i].Prev.IsZero() {
+		return false
+	}
+	if s[j].Prev.IsZero() {
+		return true
+	}
+	return s[i].Prev.Before(s[j].Prev)
+}
