79148d86cea9656e5691d810287883d2ba5ee586
Rizal Widyarta Gowandy <rizal.gow@gmail.com>
2025-04-22 14:32:48 +0700

    Upgrade go1.24

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,19 +3,23 @@
 echo -e "\e[32mRunning:\e[33m setup.\e[0m\n"
 
 echo -e "\e[32mInstalling:\e[33m air for live reload.\e[0m"
-command -v air 2>/dev/null || curl -sSfL https://raw.githubusercontent.com/cosmtrek/air/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin # v1.12.1
+command -v air 2>/dev/null || go install github.com/air-verse/air@v1.61.7
 echo ""
 
 echo -e "\e[32mInstalling:\e[33m mockgen for mock generator.\e[0m"
-command -v mockgen 2>/dev/null || GO111MODULE=off go get -v github.com/golang/mock/mockgen # v1.4.4
+command -v mockgen 2>/dev/null || go install -v github.com/golang/mock/mockgen@v1.6.0 # v1.6.0
 echo ""
 
 echo -e "\e[32mInstalling:\e[33m golangci-lint for linter.\e[0m"
-command -v golangci-lint 2>/dev/null || curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.33.0
+command -v golangci-lint 2>/dev/null || curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.1.2
 echo ""
 
 echo -e "\e[32mInstalling:\e[33m gomodifytags for generating tags.\e[0m"
-command -v gomodifytags 2>/dev/null || go install -v github.com/fatih/gomodifytags@v1.16.0
+command -v gomodifytags 2>/dev/null || go install -v github.com/fatih/gomodifytags@v1.17.0
+echo ""
+
+echo -e "\e[32mInstalling:\e[33m gotestsum.\e[0m"
+command -v gotestsum 2>/dev/null || go install -v gotest.tools/gotestsum@v1.12.1
 echo ""
 
 echo -e "\e[32mSetup:\e[33m pre-commit hook.\e[0m"
--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@
 echo -e "\e[32mRunning: \e[33mtest.\e[0m"
 
 echo -e "\e[32mType: \e[33munit test.\e[0m"
-command time -f %E go test -v -failfast -race -count=1 ./... >/dev/null || exit 1
+command time -f %E gotestsum --format=testname --hide-summary=skipped -- -failfast -count=1 -v -race ./... || exit 1
 echo -e "\e[32mUnit test: \e[33msuccess.\e[0m"
 
 echo -e "\e[32mTest: \e[33msuccess.\e[0m"
